2a90869629beabf4129e2ee7b01dadf8dd167566
Alexandru Dochitoiu <alexandru.dochitoiu@continental-corporation.com>
2022-08-23 11:35:35 +0300

    Add some logs

--- src/services/monitorLiveEvents.ts
+++ src/services/monitorLiveEvents.ts
@@ -74,39 +74,49 @@ const checkAlert = (event: IEventsStats, userAlerts: IAlert[]) => {
 };
 
 export default async function monitorLiveEvents() {
-  console.log("Monitoring live matches ... ");
-  const eventsStats = await saveEventsStats();
-  console.log(eventsStats);
-  const users = await User.find({ active: true }).exec();
-  await Promise.all(
-    users.map(async (u) => {
-      const notified = (
-        await Promise.all(
-          eventsStats.map(async (e) => {
-            if (u.subscription && checkAlert(e, u.alerts)) {
-              const payload = {
-                url: `https://www.flashscore.com/match/${e.matchInfo.id}/#/match-summary/match-statistics/0`,
-                title: `Match: ${e.matchInfo.homeTeam} - ${e.matchInfo.awayTeam}`,
-                body: `Live bet alert: ${e.matchInfo.status}' (${e.matchInfo.liveScore})`,
-              };
-              if (u.notified.indexOf(e.matchInfo.id) === -1) {
-                const result = await webpush.sendNotification(
-                  u.subscription,
-                  JSON.stringify(payload)
-                );
-                if (result.statusCode == 201) {
-                  return e.matchInfo.id;
+  let lastSetTimeoutId = null;
+  try {
+    console.log("Monitoring live matches ... ");
+    const eventsStats = await saveEventsStats();
+    console.log(eventsStats);
+    const users = await User.find({ active: true }).exec();
+    await Promise.all(
+      users.map(async (u) => {
+        const notified = (
+          await Promise.all(
+            eventsStats.map(async (e) => {
+              if (u.subscription && checkAlert(e, u.alerts)) {
+                const payload = {
+                  url: `https://www.flashscore.com/match/${e.matchInfo.id}/#/match-summary/match-statistics/0`,
+                  title: `Match: ${e.matchInfo.homeTeam} - ${e.matchInfo.awayTeam}`,
+                  body: `Live bet alert: ${e.matchInfo.status}' (${e.matchInfo.liveScore})`,
+                };
+                if (u.notified.indexOf(e.matchInfo.id) === -1) {
+                  const result = await webpush.sendNotification(
+                    u.subscription,
+                    JSON.stringify(payload)
+                  );
+                  if (result.statusCode == 201) {
+                    return e.matchInfo.id;
+                  }
                 }
               }
-            }
-          })
-        )
-      ).filter((n) => Boolean(n));
-      await User.updateOne(
-        { username: u.username },
-        { notified: [...u.notified, ...notified] }
-      );
-    })
-  );
-  await monitorLiveEvents();
+            })
+          )
+        ).filter((n) => Boolean(n));
+        await User.updateOne(
+          { username: u.username },
+          { notified: [...u.notified, ...notified] }
+        );
+      })
+    );
+    const used = process.memoryUsage().heapUsed / 1024 / 1024;
+    console.log(`The script uses approximately ${used} MB`);
+    lastSetTimeoutId = setTimeout(monitorLiveEvents, 5000);
+  } catch (err) {
+    console.error("[Error]:", err);
+    if (lastSetTimeoutId) {
+      clearTimeout(lastSetTimeoutId);
+    }
+  }
 }
